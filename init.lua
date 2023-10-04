a9cd12caaf6108f081998c2d69143a946f01a21e
Alberto Perea <eralon.dev@gmail.com>
2023-10-04 17:26:04 +0200

    Add files via upload

--- init.lua
+++ init.lua
@@ -26,6 +26,10 @@ vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '^')
 vim.keymap.set({'n', 'x', 'o'}, '<leader>l', 'g_')
 vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
 
+-- Buffer management
+vim.keymap.set('n', '<leader>n', ':bnext<cr>')
+vim.keymap.set('n', '<leader>p', ':bprev<cr>')
+
 -- Basic clipboard interaction
 vim.keymap.set({'n', 'x'}, 'gy', '"+y') -- copy
 vim.keymap.set({'n', 'x'}, 'gp', '"+p') -- paste
@@ -103,6 +107,19 @@ lazy.setup({
   {'folke/tokyonight.nvim'},
   {'kyazdani42/nvim-web-devicons'},
   {'nvim-lualine/lualine.nvim'},
+  {'tiagovla/scope.nvim'},
+  {'nvim-telescope/telescope.nvim', tag = '0.1.3',
+		dependencies = {
+			{'nvim-lua/plenary.nvim'},
+			{'burntsushi/ripgrep'},
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		}
+	},
+	{'akinsho/bufferline.nvim', version = "*"},
+	{'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+  {'preservim/nerdtree'},
+  {'startup-nvim/startup.nvim'},
+  {'neoclide/coc.nvim', branch = 'release'},
 })
 
 
@@ -129,4 +146,147 @@ require('lualine').setup({
     section_separators = '',
   },
 })
+
+---
+-- scope.nvim (buffer line)
+---
+require('scope').setup({})
+
+---
+-- telescope.nvim
+---
+require('telescope').setup({
+   defaults = {
+      vimgrep_arguments = {
+         "rg",
+         "--color=never",
+         "--no-heading",
+         "--with-filename",
+         "--line-number",
+         "--column",
+         "--smart-case",
+      },
+      prompt_prefix = "   ",
+      selection_caret = "  ",
+      entry_prefix = "  ",
+      initial_mode = "insert",
+      selection_strategy = "reset",
+      sorting_strategy = "ascending",
+      layout_strategy = "horizontal",
+      layout_config = {
+         horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+         },
+         vertical = {
+            mirror = false,
+         },
+         width = 0.87,
+         height = 0.80,
+         preview_cutoff = 120,
+      },
+      file_sorter = require("telescope.sorters").get_fuzzy_file,
+      file_ignore_patterns = {},
+      generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+      path_display = { "absolute" },
+      winblend = 0,
+      border = {},
+      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+      color_devicons = true,
+      use_less = true,
+      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+      -- Developer configurations: Not meant for general override
+      buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+   },
+   extensions = {
+      fzf = {
+         fuzzy = true, -- false will only do exact matching
+         override_generic_sorter = false, -- override the generic sorter
+         override_file_sorter = true, -- override the file sorter
+         case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+         -- the default case_mode is "smart_case"
+      },
+      media_files = {
+         filetypes = { "png", "webp", "jpg", "jpeg" },
+         find_cmd = "rg", -- find command (defaults to `fd`)
+      },
+   },
+})
+vim.keymap.set('n', '<leader>gf', ':Telescope git_files hidden=true <cr>')
+vim.keymap.set('n', '<leader>ff', ':Telescope live_grep<cr>')
+
+---
+-- bufferline
+---
+vim.opt.termguicolors = true
+require('bufferline').setup()
+vim.keymap.set('n', ',q', ':bdelete<cr>')
+
+---
+-- treesitter
+---
+require('nvim-treesitter.configs').setup({
+	ensure_installed = {
+		'lua',
+		'javascript',
+		'html',
+		'css',
+		'python',
+		'java'
+	},
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
+})
+
+---
+-- nerdtree
+---
+vim.keymap.set('n', ',e', ':NERDTreeToggle<cr>')
+vim.keymap.set('n', ',f', ':NERDTreeFind<cr>')
+
+---
+-- startup
+---
+require('startup').setup()
+
+
+---
+-- coc
+---
+local function check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
+
+vim.keymap.set("i", "C-<space>",
+    function()
+        if vim.fn['coc#pum#visible']() == 1 then
+            return vim.fn['coc#pum#next'](1)
+        end
+        if check_back_space() then
+            return vim.fn['coc#refresh']()
+        end
+        return "<Tab>"
+    end
+    , opts)
+vim.keymap.set("i", "<S-Tab>", function()
+        if vim.fn['coc#pum#visible']() == 1 then
+            return vim.fn['coc#pum#prev'](1)
+        end
+        return "<S-Tab>"
+end, opts)
+vim.keymap.set("i", "<CR>", function()
+        if vim.fn['coc#pum#visible']() == 1 then
+            return vim.fn['coc#pum#confirm']();
+        end
+       return "\r"
+end, opts)
 
